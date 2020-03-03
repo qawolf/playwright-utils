54fe900ba468d5fa0299e84922fdf22d8520f1f9
Jon Perl <perl.jonathan@gmail.com>
2020-03-03 12:20:27 -0700

    make forEachPage pageFn more permissive

--- src/forEachPage.ts
+++ src/forEachPage.ts
@@ -2,10 +2,11 @@ import { BrowserContext, Page } from 'playwright-core';
 
 export const forEachPage = async (
   context: BrowserContext,
-  pageFn: (page: Page) => void | Promise<void>,
+  pageFn: (page: Page) => any,
 ): Promise<void> => {
   // XXX remove cast after playwright fixes types
   (context as any).on('page', async event => pageFn(await event.page()));
+
   const pages = await context.pages();
   await Promise.all([pages.map(page => pageFn(page))]);
 };
