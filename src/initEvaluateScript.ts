df7ced1802acb8acc8de9d99dce7fb0b722b802a
Jon Perl <perl.jonathan@gmail.com>
2020-03-03 11:33:00 -0700

    add API to readme

--- src/initEvaluateScript.ts
+++ src/initEvaluateScript.ts
@@ -3,11 +3,12 @@ import { Page } from 'playwright-core';
 export const initEvaluateScript = async (
   page: Page,
   script: string | Function,
+  ...args: any[]
 ): Promise<unknown> => {
   const [, result] = await Promise.all([
-    page.addInitScript(script),
+    page.addInitScript(script, ...args),
     // TODO fix PageFunction cast
-    page.evaluate(script as any),
+    page.evaluate(script as any, ...args),
   ]);
 
   return result;
