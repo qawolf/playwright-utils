e9b7465a46ce3e6b710a710bed49182355f7e296
Jon Perl <perl.jonathan@gmail.com>
2020-03-18 20:32:11 -0600

    openScreenshot: test the screenshot was created

--- src/page/openScreenshot.ts
+++ src/page/openScreenshot.ts
@@ -1,10 +1,10 @@
 import { ChildProcess } from 'child_process';
 import open from 'open';
 import { Page } from 'playwright';
-import * as tempy from 'tempy';
+import { file } from 'tempy';
 
 export const openScreenshot = async (page: Page): Promise<ChildProcess> => {
-  const path = tempy.file({ extension: 'png' });
+  const path = file({ extension: 'png' });
   await page.screenshot({ path });
   const process = await open(path);
   return process;
